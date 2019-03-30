e47d0ea7e4d279eb91d6f3428c34011e5a3a26de
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-03-30 21:52:00 +0800

    Added food cache to optimize food_load()

--- food.py
+++ food.py
@@ -1,4 +1,5 @@
-def search_food(query:str, food_dict=load_food()):
+def search_food(query:str):
+    food_dict = load_food()
     results = []
     for item in food_dict.items():
         if not item.find(query) == -1:
@@ -27,26 +28,37 @@ def calorie_parser(calorie: str):
     return int(calorie_count), unit_calorie.lstrip('(')
 
 
+
+
+_food_cache = dict()
+
 def load_food():
-    with open('food.csv', 'r') as food:
-        food_base = food.readlines()
-    food_dict = {}
-    mode_read = False
-    mode = ""
-    for line in food_base:
-        line = line.strip('\n')
-        if line == '--':
-            mode_read = True
-        elif mode_read:
-            mode = line
-            mode_read = False
-        elif not line == '':
-            cols = line.split(',')
-            new_food = dict()
-            new_food["Unit Calorie"], new_food["Unit"] = calorie_parser(cols[1])
-            new_food["Status"] = cols[3]
-            new_food["Mode"] = mode
-            name = cols[0]
-            food_dict[name] = new_food
+    global _food_cache
+    if len(_food_cache) == 0:
+        with open('food.csv', 'r') as food:
+            food_base = food.readlines()
+        food_dict = {}
+        mode_read = False
+        mode = ""
+        for line in food_base:
+            line = line.strip('\n')
+            if line == '--':
+                mode_read = True
+            elif mode_read:
+                mode = line
+                mode_read = False
+            elif not line == '':
+                cols = line.split(',')
+                new_food = dict()
+                new_food["Unit Calorie"], new_food["Unit"] = calorie_parser(cols[1])
+                new_food["Status"] = cols[3]
+                new_food["Mode"] = mode
+                name = cols[0]
+                food_dict[name] = new_food
+        _food_cache = food_dict
+    # Load cache if not empty
+    else:
+        food_dict = _food_cache
+        
     return food_dict
 
