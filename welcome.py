132686a090ad95d024e634b1d682a1977583e5f1
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-03-31 21:18:15 +0800

    Added date input checking

--- welcome.py
+++ welcome.py
@@ -1,6 +1,17 @@
 from fitness_calc import *
 import os
 from user import write_user_data
+import day_tracking
+
+
+def input_date(prompt, error_msg):
+    date = input(prompt)
+    try:
+        day_tracking.create_date(date)
+    except ValueError:
+        print(error_msg)
+        return input_date(prompt, error_msg)
+    return date
 
 
 def new_user_data(user_info):
@@ -13,14 +24,15 @@ def new_user_data(user_info):
                 Chrome\Application\chrome.exe" https://www.vertex42.com/ExcelTemplates/Images/body-mass-index-chart.gif')
     print("Use the image as a guide...")
     desired_weight = input("What is your desired weight: ")
-    days = input("When do you want to achieve this?")
+    date = input_date("When do you want to achieve this? (mm/dd/YYYY)", "Invalid Date")
+
     user_info['Weight'] = weight
     user_info['Height'] = height
     user_info['Sex'] = sex
     user_info['Age'] = age
     user_info['BMI'] = calculate_bmi(weight, height)
     user_info['BMR'] = calculate_bmr(weight, height, sex, age)
-    user_info['DesiredWeight'] = desired_weight
-    user_info['Goal Date'] = days
+    user_info['Desired Weight'] = desired_weight
+    user_info['Goal Date'] = date
     write_user_data(user_info)
 
