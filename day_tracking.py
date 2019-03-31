22962e92ad3b88a8fa885fd3eee67f88327aac69
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-03-31 11:39:35 +0800

    Added date and food file management

--- day_tracking.py
+++ day_tracking.py
@@ -1,1 +1,33 @@
+import food
 import datetime
+_set_date = datetime.datetime.now()
+"""
+    Foodfile Format:
+    food_name,mode
+    [Separated by a comma]
+    mode is an element of {Breakfast, Lunch, Dinner, Snacks}
+
+"""
+def change_set_date(year, month, day):
+    global _set_date
+    _set_date = datetime.datetime(year, month, day)
+
+def get_set_date_text():
+    return _set_date.strftime("%Y-%b-%d")
+
+def access_date_file(user_info, file_mode):
+    username = user_info['User']
+    date_str = get_set_date_text()
+    date_file = "Users\\{0}\\{1}.data".format(username, date_str)
+    return open(date_file, file_mode)
+    
+def add_food_to_set_date(food_name, mode, user_info):
+    """
+        food_name: refers to the name of the food as specified by food_dict
+        mode: Breakfast, Lunch, Dinner, Snacks
+    """
+    date_file = access_date_file(user_info, 'a')
+    date_file.write(f"{food_name},{mode}")
+    date_file.close()
+
+
