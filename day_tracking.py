a0b88b500eb9221b78c45f6b7f5e82a78c6e644e
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-03-31 19:20:55 +0800

    Added weight tracking

--- day_tracking.py
+++ day_tracking.py
@@ -3,17 +3,27 @@ import datetime
 _set_date = datetime.datetime.now()
 """
     Foodfile Format:
-    food_name,mode
+    type:food_name,amount,mode
     [Separated by a comma]
     mode is an element of {Breakfast, Lunch, Dinner, Snacks}
-
+    type is either food or weight
 """
+
+def snap_date_to_now():
+    global _set_date
+    _set_date = datetime.datetime.now()
+    
 def change_set_date(year, month, day):
     global _set_date
     _set_date = datetime.datetime(year, month, day)
 
 def get_set_date_text():
+    global _set_date
     return _set_date.strftime("%Y-%b-%d")
+
+def get_time_text():
+    global _set_date
+    return _set_date.strftime("%H:%M:%S")
 
 def access_date_file(user_info, file_mode):
     username = user_info['User']
@@ -28,7 +38,12 @@ def add_food_to_set_date(food_name, amount, mode, user_info):
         amount: amount of food (according to the unit in the database
     """
     date_file = access_date_file(user_info, 'a')
-    date_file.write(f"{food_name},{amount},{mode}")
+    date_file.write(f"food:{food_name},{amount},{mode}")
     date_file.close()
 
+def add_new_weight(weight, user_info):
+    date_file = access_date_file(user_info, 'a')
+    snap_date_to_now()
+    time = get_time_text()
+    date_file.write(f"weight:{weight},{time}")
 
