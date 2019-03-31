c5b0f845000437ab6dbd8c8cab03d5129dc36e5a
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-03-31 11:42:27 +0800

    Added amount in food adding function

--- day_tracking.py
+++ day_tracking.py
@@ -21,13 +21,14 @@ def access_date_file(user_info, file_mode):
     date_file = "Users\\{0}\\{1}.data".format(username, date_str)
     return open(date_file, file_mode)
     
-def add_food_to_set_date(food_name, mode, user_info):
+def add_food_to_set_date(food_name, amount, mode, user_info):
     """
         food_name: refers to the name of the food as specified by food_dict
         mode: Breakfast, Lunch, Dinner, Snacks
+        amount: amount of food (according to the unit in the database
     """
     date_file = access_date_file(user_info, 'a')
-    date_file.write(f"{food_name},{mode}")
+    date_file.write(f"{food_name},{amount},{mode}")
     date_file.close()
 
 
