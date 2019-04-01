8f2c6bc91fcdc228058d4f6b203a88368f1d9db9
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-04-01 18:14:49 +0800

    Added food menu

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ import user
 import os
 import time
 import day_tracking
+import food
 
 
 def main_menu(user_info):
@@ -14,17 +15,60 @@ def main_menu(user_info):
     choice = input("\t> ")
     if choice == '1':
         user.signup()
-        welcome.new_user_data(user_info)
         print("Sign up complete...")
         time.sleep(5)
         # os.system("cls")
         main_menu(user_info)
     elif choice == '2':
         user.login(user_info)
+        user.fetch_user_info(user_info)
+        if len(user_info) == 1:
+            welcome.new_user_data(user_info)
     else:
         print("Invalid Output...")
         main_menu(user_info)
     return
+
+
+def display_food(name, counter):
+    food_data = food.get_food_data(name)
+    calories, unit = food_data["Unit Calorie"], food_data["Unit"]
+    print("{0}-{1} ({2} calories per {3}".format(counter, name, calories, unit))
+
+
+def search_food_menu(user_info):
+    print("Search for food: ", end=" ")
+    query = input()
+    results = food.search_food(query)
+    for index, food_name in enumerate(results):
+        display_food(food_name, index + 1)
+    if len(results) > 0:
+        choice = int(input("Choose a result: "))
+        return results[choice - 1]
+    else:
+        print("No results found.")
+        return None
+
+
+def add_food_menu(user_info):
+    food_name = search_food_menu(user_info)
+    food_data = food.get_food_data(food_name)
+    amount = float(input("Amount (in {0}): ".format(food_data["Unit"])))
+    day_tracking.add_food_to_set_date(food_name, amount, "Breakfast", user_info)  # have user choose mode later
+
+
+def food_menu(user_info):
+    _,food_data = day_tracking._get_date_data(user_info)
+    counter = 1
+    print("Max Calories: {0}".format(day_tracking.get_allowed_calories_today(user_info)))
+    for name, params in food_data.items():
+        calorie_amount = food.get_calorie(name, float(params["Amount"]))
+        print("{0}. {1} | {2} | {3}".format(counter, name, params["Amount"], calorie_amount))
+        counter += 1
+    print("Food Menu:")
+    print("What would you like to do?")
+    print("\t 1. Add New Food")
+    choice = input("\t> ")
 
 
 def user_menu(user_info):
@@ -33,6 +77,11 @@ def user_menu(user_info):
     print(">> Current Date: {0}".format(day_tracking.get_set_date_text()))
     print("\t1. Check Food Balance")
     print("\t2. Add New Food")
-    print("\t3.Change Personal Info")
+    print("\t3. Record New Weight")
+    print("\t4.Change Personal Info")
     choice = input("\t> ")
+    if choice == '1':
+        food_menu(user_info)
+    if choice == '2':
+        add_food_menu(user_info)
 
