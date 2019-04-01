2b587744f738b7bedfd8ba21926849eeaffdb911
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-04-01 20:04:11 +0800

    Added weight and date change menu

--- display.py
+++ display.py
@@ -4,6 +4,7 @@ import os
 import time
 import day_tracking
 import food
+import fitness_calc
 
 
 def main_menu(user_info):
@@ -57,6 +58,24 @@ def add_food_menu(user_info):
     day_tracking.add_food_to_set_date(food_name, amount, "Breakfast", user_info)  # have user choose mode later
 
 
+def add_weight_menu(user_info):
+    weight = float(fitness_calc.lexical_metric_conversion(input("Record your weight today: ")))
+    day_tracking.add_new_weight(weight, user_info)
+
+
+def weight_menu(user_info):
+    print("Weight Menu:")
+    print("What would you like to do?")
+    print("Set Date: {0}".format(day_tracking.get_set_date_text()))
+    print("\t1. Add new weight recording to this date.")
+    print("\tX. Go back")
+    choice = input("\t>")
+    if choice == '1':
+        add_weight_menu(user_info)
+    elif choice == 'X':
+        user_menu(user_info)
+
+
 def food_menu(user_info):
     _,food_data = day_tracking._get_date_data(user_info)
     counter = 1
@@ -79,19 +98,36 @@ def food_menu(user_info):
         food_menu(user_info)
 
 
+def change_date_menu(user_info):
+    date = input("Type in the new date: ")
+    try:
+        new_date = day_tracking.create_date(date)
+        day_tracking.change_set_date_with_datetime(new_date)
+    except ValueError:
+        print("Invalid date.")
+        change_date_menu(user_info)
+
+
 def user_menu(user_info):
     print("Hello, {0}".format(user_info["User"]))
     print("What would you like to do?")
-    print(">> Current Date: {0}".format(day_tracking.get_set_date_text()))
+    print(">> Current Viewing Date: {0}".format(day_tracking.get_set_date_text()))
     print("\t1. Check Food Balance")
     print("\t2. Add New Food")
     print("\t3. Record New Weight")
     print("\t4.Change Personal Info")
+    print("\t5. Check another date.")
     choice = input("\t> ")
     if choice == '1':
         food_menu(user_info)
-    if choice == '2':
+    elif choice == '2':
         add_food_menu(user_info)
+    elif choice == '3':
+        weight_menu(user_info)
+    elif choice == '4':
+        welcome.new_user_data(user_info)
+    elif choice == '5':
+        change_date_menu(user_info)
     else:
         print("Invalid choice.")
         user_menu(user_info)
