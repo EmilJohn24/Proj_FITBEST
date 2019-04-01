b467458c5a6097d94882d94a44e3fa3dc7cd90b5
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-04-01 22:09:54 +0800

    Added clear screen decorator(di pa naturo pero wink wink)

--- display.py
+++ display.py
@@ -5,8 +5,18 @@ import time
 import day_tracking
 import food
 import fitness_calc
+from functools import wraps
 
 
+def clear_screen(menu_func):
+    @wraps(menu_func)
+    def clear_wrapper(*args, **kwargs):
+        os.system("cls")
+        return menu_func(*args, **kwargs)
+    return clear_wrapper
+
+
+@clear_screen
 def main_menu(user_info):
     print("Welcome to FITBEST!")
     print("What would you like to do?")
@@ -33,21 +43,24 @@ def main_menu(user_info):
     return
 
 
+@clear_screen
 def add_food_to_database(user_info):
     name = input("Name of the food: ")
-    unit = input("Unit of calories")
-    calorie = float(input("Number of calories per unit"))
-    food.add_food_to_database(name, unit, calorie)
-    foodbase = food.load_food()
-    return foodbase[name]
+    unit = input("Unit of calories: ")
+    calorie = float(input("Number of calories per unit: "))
+    food.add_food_to_database(name, calorie, unit)
+    food_base = food.load_food()
+    return name
 
 
+@clear_screen
 def display_food(name, counter):
     food_data = food.get_food_data(name)
     calories, unit = food_data["Unit Calorie"], food_data["Unit"]
     print("{0}-{1} ({2} calories per {3}".format(counter, name, calories, unit))
 
 
+@clear_screen
 def search_food_menu(user_info):
     print("Search for food: ", end=" ")
     query = input()
@@ -70,19 +83,23 @@ def search_food_menu(user_info):
             return search_food_menu(user_info)
 
 
+@clear_screen
 def add_food_menu(user_info):
     food_name = search_food_menu(user_info)
     food_data = food.get_food_data(food_name)
+    print("Adding {0}".format(food_name))
     amount = float(input("Amount (in {0}): ".format(food_data["Unit"])))
     day_tracking.add_food_to_set_date(food_name, amount, "Breakfast", user_info)  # have user choose mode later
 
 
+@clear_screen
 def add_weight_menu(user_info):
     weight = float(fitness_calc.lexical_metric_conversion(input("Record your weight today: ")))
     day_tracking.add_new_weight(weight, user_info)
     user_menu(user_info)
 
 
+@clear_screen
 def weight_menu(user_info):
     print("Weight Menu:")
     print("What would you like to do?")
@@ -96,6 +113,7 @@ def weight_menu(user_info):
         user_menu(user_info)
 
 
+@clear_screen
 def display_info(user_info):
     side_by_side = False
     for label, info in user_info.items():
@@ -104,33 +122,42 @@ def display_info(user_info):
         else:
             label_end = '\n'
 
-        print(f"{label}: {info[0:8]:<30}", end=label_end)
+        print(f"{label}: {info[0:10]:<30}", end=label_end)
         side_by_side = not side_by_side
 
 
+@clear_screen
 def food_menu(user_info):
-    _, food_data = day_tracking._get_date_data(user_info)
+    try:
+        _, food_data = day_tracking._get_date_data(user_info)
+    except FileNotFoundError:
+        print("No food to display...")
     counter = 1
     print("Max Calories: {0:.2f}".format(day_tracking.get_allowed_calories_today(user_info)))
-    print("Remaining Calories: {0:.2f}".format(day_tracking.get_remaining_calories_today(user_info)))
-    for name, params in food_data.items():
-        calorie_amount = food.get_calorie(name, float(params["Amount"]))
-        print("{0}. {1} | {2} | {3}".format(counter, name, params["Amount"], calorie_amount))
-        counter += 1
-    print("Food Menu:")
-    print("What would you like to do?")
-    print("\t 1. Add New Food")
-    print("\t X. Go Back")
-    choice = input("\t> ")
-    if choice == '1':
-        add_food_menu(user_info)
-    elif choice == 'X':
-        user_menu(user_info)
+    if food_data:
+        print("Remaining Calories: {0:.2f}".format(day_tracking.get_remaining_calories_today(user_info)))
+        for name, params in food_data.items():
+            calorie_amount = food.get_calorie(name, float(params["Amount"]))
+            print("{0}. {1} | {2} | {3}".format(counter, name, params["Amount"], calorie_amount))
+            counter += 1
+        print("Food Menu:")
+        print("What would you like to do?")
+        print("\t 1. Add New Food")
+        print("\t X. Go Back")
+        choice = input("\t> ")
+        if choice == '1':
+            add_food_menu(user_info)
+        elif choice == 'X':
+            user_menu(user_info)
+        else:
+            print("Invalid choice")
+            food_menu(user_info)
     else:
-        print("Invalid choice")
-        food_menu(user_info)
+        print("No food to display.")
+    user_menu(user_info)
 
 
+@clear_screen
 def change_date_menu(user_info):
     date = input("Type in the new date: ")
     try:
@@ -142,6 +169,7 @@ def change_date_menu(user_info):
         change_date_menu(user_info)
 
 
+@clear_screen
 def user_menu(user_info):
     user.fetch_user_info(user_info)
     print("Hello, {0}".format(user_info["User"]))
@@ -167,4 +195,5 @@ def user_menu(user_info):
     else:
         print("Invalid choice.")
         user_menu(user_info)
+    user_menu(user_info)
 
