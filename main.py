545cd336bbe8025c783ed382caa20b1edf96cb05
Emil John Lopez <emil_lopez@dlsu.edu.ph>
2019-04-03 19:57:37 +0800

    Turned main contents into a function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import display
-
-if __name__ == "__main__":
+# if __name__ == "__main__":
+def main():
     user_info = dict()
     display.main_menu(user_info)
     display.user_menu(user_info)
+
+
+if __name__ == "__main__":
+    main()
